20e28a55eb22a08e5d4bf4786cb075ab33e4a11a
Chinmay Arvind <chinmayarvind23@gmail.com>
2024-10-11 14:46:45 -0700

    project build errors fixing commit

--- ygraph-ai-smartfox-client/src/main/java/ygraph/ai/smartfox/games/GameTimer.java
+++ ygraph-ai-smartfox-client/src/main/java/ygraph/ai/smartfox/games/GameTimer.java
@@ -1,46 +1,46 @@
-// package ygraph.ai.smartfox.games;
+package ygraph.ai.smartfox.games;
 
-// import java.util.TimerTask;
+import java.util.TimerTask;
 
-// /**
-//  * An implementation of java.util.TimerTask for timeout notification.
-//  * Used by GameClient. 
-//  * For COSC322 project purpose, you do not need to use this class.
-//  *  
-//  * @author yong.gao@ubc.ca
-//  *
-//  */
+/**
+ * An implementation of java.util.TimerTask for timeout notification.
+ * Used by GameClient. 
+ * For COSC322 project purpose, you do not need to use this class.
+ *  
+ * @author yong.gao@ubc.ca
+ *
+ */
 
-// public class GameTimer extends TimerTask {
+public class GameTimer extends TimerTask {
 
-// 	public static final int TIMEOUT = 30000; // in milliseconds 
+	public static final int TIMEOUT = 30000; // in milliseconds 
 	
 	
-// 	private GamePlayer delegate = null;
-// 	private GameClient gameClient = null;
+	private GamePlayer delegate = null;
+	private GameClient gameClient = null;
 	
-// 	private int violationCount = 0;
+	private int violationCount = 0;
 	
-// 	public GameTimer(GamePlayer delegate, GameClient gameClient, int numOfTimeOut){
-// 		this.delegate = delegate;	
-// 		this.gameClient = gameClient;
-// 		this.violationCount = numOfTimeOut;
-// 	}
+	public GameTimer(GamePlayer delegate, GameClient gameClient, int numOfTimeOut){
+		this.delegate = delegate;	
+		this.gameClient = gameClient;
+		this.violationCount = numOfTimeOut;
+	}
 	
 	
-// 	/**
-// 	 * call GamePlayer's method handleMessage(String msg) when timeout 
-// 	 */
-// 	public void run() {
+	/**
+	 * call GamePlayer's method handleMessage(String msg) when timeout 
+	 */
+	public void run() {
 		
-// 		violationCount++;
-// 		gameClient.updateTimeOut();
+		violationCount++;
+		gameClient.updateTimeOut();
 		
-//         try {
-// 			delegate.handleMessage("Timeout Count = " + violationCount + " !!!");
-// 		} catch (Exception e) {
-// 			e.printStackTrace();
-// 		}
-// 	}
+        try {
+			delegate.handleMessage("Timeout Count = " + violationCount + " !!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-// }
+}
--- ygraph-ai-smartfox-client/src/main/java/ygraph/ai/smartfox/games/Spectator.java
+++ ygraph-ai-smartfox-client/src/main/java/ygraph/ai/smartfox/games/Spectator.java
@@ -1,102 +1,102 @@
-// package ygraph.ai.smartfox.games;
+package ygraph.ai.smartfox.games;
 
-// import java.util.ArrayList;
-// import java.util.Map;
+import java.util.ArrayList;
+import java.util.Map;
 
-// import ygraph.ai.smartfox.games.amazons.AmazonsGameMessage;
+import ygraph.ai.smartfox.games.amazons.AmazonsGameMessage;
 
-// public class Spectator extends GamePlayer{
+public class Spectator extends GamePlayer{
 
-// 	private String userName = null;
+	private String userName = null;
 	
-// 	private BaseGameGUI gamegui = null;
-// 	private GameClient gameClient = null;
+	private BaseGameGUI gamegui = null;
+	private GameClient gameClient = null;
 	
-// 	public Spectator() {
-// 		gamegui = new BaseGameGUI(this);
-// 	}
+	public Spectator() {
+		gamegui = new BaseGameGUI(this);
+	}
 
-// 	@Override
-// 	public void onLogin() {
-//         userName = gameClient.getUserName();
+	@Override
+	public void onLogin() {
+        userName = gameClient.getUserName();
                
-//         if(gamegui != null) {                
-//         	gamegui.setRoomInformation(gameClient.getRoomList()); 
-//         }
-// 	}
+        if(gamegui != null) {                
+        	gamegui.setRoomInformation(gameClient.getRoomList()); 
+        }
+	}
 
-// 	@Override
-// 	public boolean handleGameMessage(String messageType, Map<String, Object> msgDetails) {
-// 		if(messageType.equals(GameMessage.GAME_STATE_BOARD)){
-// 			ArrayList<Integer> gameS = (ArrayList<Integer>) msgDetails.get("game-state");
-// 			if(getGameGUI() != null) {
-// 				getGameGUI().setGameState(gameS);
-// 			}
-// 		}
-// 		else if(messageType.equals(GameMessage.GAME_ACTION_MOVE)){
-// 			//System.out.println(msgDetails.get(AmazonsGameMessage.QUEEN_POS_CURR));
-// 	    	gamegui.updateGameState(msgDetails);
-// 			//handleOpponentMove(msgDetails);
-// 	    }
+	@Override
+	public boolean handleGameMessage(String messageType, Map<String, Object> msgDetails) {
+		if(messageType.equals(GameMessage.GAME_STATE_BOARD)){
+			ArrayList<Integer> gameS = (ArrayList<Integer>) msgDetails.get("game-state");
+			if(getGameGUI() != null) {
+				getGameGUI().setGameState(gameS);
+			}
+		}
+		else if(messageType.equals(GameMessage.GAME_ACTION_MOVE)){
+			//System.out.println(msgDetails.get(AmazonsGameMessage.QUEEN_POS_CURR));
+	    	gamegui.updateGameState(msgDetails);
+			//handleOpponentMove(msgDetails);
+	    }
  		
-// 		//getGameClient().leaveCurrentRoom();
-// 		//getGameClient().logout();
+		//getGameClient().leaveCurrentRoom();
+		//getGameClient().logout();
 		
-// 		return false;
-// 	}
+		return false;
+	}
 
-// 	@Override
-// 	public GameClient getGameClient() {
-// 		return this.gameClient;
-// 	}  
+	@Override
+	public GameClient getGameClient() {
+		return this.gameClient;
+	}  
 
-// 	@Override
-// 	public BaseGameGUI getGameGUI() {
-// 		return this.gamegui;
-// 	}
+	@Override
+	public BaseGameGUI getGameGUI() {
+		return this.gamegui;
+	}
 	
-// 	/**
-// 	 * 
-// 	 */
-// 	public void connect() {
-// 		// create a game client and use "this" class (a GamePlayer) as the delegate.
-// 		// the game client client take care of the communication with the server.
+	/**
+	 * 
+	 */
+	public void connect() {
+		// create a game client and use "this" class (a GamePlayer) as the delegate.
+		// the game client client take care of the communication with the server.
 		
-// 		//use empty user name and passwd. The system will assign one for you
-// 		gameClient = new GameClient("", "", this);
-// 	}
+		//use empty user name and passwd. The system will assign one for you
+		gameClient = new GameClient("", "", this);
+	}
 	
-// 	@Override
-// 	public String userName() {
-// 		return this.userName;
-// 	}
+	@Override
+	public String userName() {
+		return this.userName;
+	}
 	
-//     public static void main(String args[]) {
+    public static void main(String args[]) {
         
-//         try {
-//              for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                  if ("Nimbus".equals(info.getName())) {
-//                      javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                      break;
-//                  }
-//              }
-//          } catch (ClassNotFoundException ex) {
-//              java.util.logging.Logger.getLogger(BaseGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//          } catch (InstantiationException ex) {
-//              java.util.logging.Logger.getLogger(BaseGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//          } catch (IllegalAccessException ex) {
-//              java.util.logging.Logger.getLogger(BaseGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//          } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//              java.util.logging.Logger.getLogger(BaseGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//          }
+        try {
+             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                 if ("Nimbus".equals(info.getName())) {
+                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                     break;
+                 }
+             }
+         } catch (ClassNotFoundException ex) {
+             java.util.logging.Logger.getLogger(BaseGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         } catch (InstantiationException ex) {
+             java.util.logging.Logger.getLogger(BaseGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         } catch (IllegalAccessException ex) {
+             java.util.logging.Logger.getLogger(BaseGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+             java.util.logging.Logger.getLogger(BaseGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         }
                  
                
-//          java.awt.EventQueue.invokeLater(new Runnable() {
-//              public void run() {
-//             	 Spectator s = new Spectator();
-//             	 s.Go();
-//              }
-//          });        
-//      }//end of main()
+         java.awt.EventQueue.invokeLater(new Runnable() {
+             public void run() {
+            	 Spectator s = new Spectator();
+            	 s.Go();
+             }
+         });        
+     }//end of main()
     	
-// }//end of class
+}//end of class
